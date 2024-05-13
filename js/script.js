38c3a31dc748545e6fedaa130e37776498182c6f
Zosia Samosia <Zosia@Samosia.pl>
2024-05-13 12:36:42 +0200

    add header menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-"use script"
+'use strict';
+
+const hamburger = document.querySelector('.material-icons');
+const menu = document.querySelector('.navigation');
+const open = document.querySelector('nav .material-icons');
+const close = document.querySelector('.navigation .material-icons');
+
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('showMenu')
+    open.classList.add('material-icons-open')
+})
+
+close.addEventListener('click', () => {
+    menu.classList.remove('showMenu')
+    open.classList.remove('material-icons-open')
+})
